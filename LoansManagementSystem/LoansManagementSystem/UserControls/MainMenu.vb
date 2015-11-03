016ad10e5d328b701e5d13c7249f86b4288b0e9e
eom-eom <elbertmolina_25@hotmail.com>
2015-11-03 10:55:55 +0800

    main menu again

--- LoansManagementSystem/LoansManagementSystem/UserControls/MainMenu.vb
+++ LoansManagementSystem/LoansManagementSystem/UserControls/MainMenu.vb
@@ -182,6 +182,6 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'frmReports.Show()
+        frmReports.Show()
     End Sub
 End Class
