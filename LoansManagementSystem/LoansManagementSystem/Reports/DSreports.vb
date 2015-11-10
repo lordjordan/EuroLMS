4ab5bfc1e0344602ad2a9c61f49a4d26508338b3
eom-eom <elbertmolina_25@hotmail.com>
2015-11-10 17:14:49 +0800

    ds

--- LoansManagementSystem/LoansManagementSystem/Reports/DSreports.vb
+++ LoansManagementSystem/LoansManagementSystem/Reports/DSreports.vb
@@ -1,11 +1,23 @@
 ﻿Partial Class DSreports
-    Partial Class PaymentDataTable
+    Partial Class Detailed_ReportDataTable
 
-        Private Sub PaymentDataTable_PaymentRowChanging(sender As Object, e As PaymentRowChangeEvent) Handles Me.PaymentRowChanging
+        Private Sub Detailed_ReportDataTable_Detailed_ReportRowChanging(sender As Object, e As Detailed_ReportRowChangeEvent) Handles Me.Detailed_ReportRowChanging
+
+        End Sub
+
+       
+
+    End Class
+
+    Partial Class SOADataTable
+
+        Private Sub SOADataTable_SOARowChanging(sender As Object, e As SOARowChangeEvent) Handles Me.SOARowChanging
 
         End Sub
 
     End Class
+
+
 
     Partial Class BranchDataTable
 
